b115aeae7a8963416d578cec4b299c6938edfdba
SNelson723 <stephenn649@gmail.com>
2025-01-08 23:15:28 -0600

    Added some comment notes to keep tabs on what is going on for future study

--- server/server.js
+++ server/server.js
@@ -19,13 +19,17 @@ const server = viteExpress.listen(app, PORT, () => {
   console.log(`Server is listening at ${PORT}`);
 });
 
+// then create a socket.io => a new one is created on a visit to the site => can have multiple
 const io = new Server(server);
 
 // Socket.IO events
 io.on('connection', (socket) => {
   // console.log('A user connected');
   socket.emit('chat-message', 'Hello World');
+
+  // this is working
+  // the message is the state handled on the client side and sent here on submit
   socket.on('send-chat-message', message => {
-    console.log(message)
+    console.log(message);
   });
 });
